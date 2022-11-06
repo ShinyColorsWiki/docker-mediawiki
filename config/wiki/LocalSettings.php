7be5fc2e605c8a3554308673e41fe1557a936e40
MPThLee <mpthlee@gmail.com>
2022-11-06 16:30:52 +0900

    fix: :construction: CF and Docker and Internal Server [ci skip]

--- config/wiki/LocalSettings.php
+++ config/wiki/LocalSettings.php
@@ -168,8 +168,30 @@ $wgCookieDomain = ".shinycolors.wiki"; # this should have to.
 $wgUseCdn = true;
 #$wgCdnServers = array();
 #$wgCdnServers[] = "127.0.0.1:6081";
-$wgCdnServersNoPurge = array( # docker ips
-        "172.16.0.0/12"
+$wgCdnServersNoPurge = array( # list of CloudFlare ipv4 list and docker ips.
+        "172.16.0.0/12",
+        "173.245.48.0/20",
+        "103.21.244.0/22",
+        "103.22.200.0/22",
+        "103.31.4.0/22",
+        "141.101.64.0/18",
+        "108.162.192.0/18",
+        "190.93.240.0/20",
+        "188.114.96.0/20",
+        "197.234.240.0/22",
+        "198.41.128.0/17",
+        "162.158.0.0/15",
+        "172.64.0.0/13",
+        "131.0.72.0/22",
+        "104.16.0.0/13",
+        "104.24.0.0/14",
+        "2400:cb00::/32",
+        "2606:4700::/32",
+        "2803:f800::/32",
+        "2405:b500::/32",
+        "2405:8100::/32",
+        "2a06:98c0::/29",
+        "2c0f:f248::/32"
 );
 
 # Favicon & Touch Icon
@@ -206,6 +228,9 @@ $wgAutopromote = array(
         "emailconfirmed" => APCOND_EMAILCONFIRMED,
 );
 $wgImplicitGroups[] = 'emailconfirmed';
+
+# Set internal server.
+$wgInternalServer = 'http://http:8080';
 
 # Load Extension settings config.
 require_once '/setting/wiki/ExtensionSettings.php';
